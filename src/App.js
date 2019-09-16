40847adf3f90fb42c6b6882b37a60f250817cb1c
Oluwajoba <oluwajobabello@gmail.com>
2019-09-16 17:10:40 +0100

    add back-button to home component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 
 function Home(props) {
-  return <h1>Home Component</h1>
+  return <h1>Home Component</h1>;
 }
 function About(props) {
-  return <h1>About Component</h1>
+  const { push } = props.history;
+  return(
+  <>
+    <h1>About Component</h1>
+    <button onClick={() => push("/")}>Back Home</button>
+  </>
+  )
 }
 function Contact(props) {
-  return <h1>Contact Component</h1>
+  return <h1>Contact Component</h1>;
 }
 
 
@@ -19,14 +25,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
 
-        
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
         </BrowserRouter>
       </header>
     </div>
