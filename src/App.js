5ec263c51c200274cbec3ea96f0ba9ffd10dc526
Oluwajoba <oluwajobabello@gmail.com>
2019-09-16 15:49:53 +0100

    add links for home, about, and contact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Router, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        </BrowserRouter>
       </header>
     </div>
   );
