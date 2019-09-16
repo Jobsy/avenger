e68faddb591e969980afe5805320d4cce6e87aba
Oluwajoba <oluwajobabello@gmail.com>
2019-09-16 15:54:30 +0100

    create functions for home, about, and contact

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
 import './App.css';
 import { BrowserRouter, Router, Link } from 'react-router-dom';
+
+
+function home(props) {
+  return <h1>Home Component</h1>
+}
+function about(props) {
+  return <h1>About Component</h1>
+}
+function contact(props) {
+  return <h1>Contact Component</h1>
+}
+
 
 function App() {
   return (
